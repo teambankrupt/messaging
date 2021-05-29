49bd7d92ebf08187903b8cecb418dd1543ca7d8d
Sayem Hossain <sayemkcn@gmail.com>
2021-05-29 22:24:32 +0600

    [Add] chat css, fix in websocket config

--- src/main/java/com/example/messaging/configs/WebSocketConfig.java
+++ src/main/java/com/example/messaging/configs/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
+//        registry.addEndpoint("/chat");
         registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
 
