06f83e509e7ce151fb53d097f71a77ae9ce345e8
Sayem Hossain <mail@sayem.dev>
2023-12-04 00:27:37 +0600

    temp

--- src/main/java/com/example/messaging/configs/WebSocketConfig.java
+++ src/main/java/com/example/messaging/configs/WebSocketConfig.java
@@ -18,8 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
 }
